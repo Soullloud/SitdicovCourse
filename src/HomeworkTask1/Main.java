be710cf62919a10f5be3cfe279284a50aff04703
Soullloud <97365214+Soullloud@users.noreply.github.com>
2024-02-26 21:17:48 +0300

    Доделал класс Channel Сделал класс Program, Controller Написал логику включения рандомного канала и рандомной передачи

--- src/HomeworkTask1/Main.java
+++ src/HomeworkTask1/Main.java
@@ -4,16 +4,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        TV tv = new TV("Samsung");
-        Channel channel = new Channel("Первый");
-        Program p1 = new Program("Давай поженимся");
-        Program p2 = new Program("Малахов+");
-        Program p3 = new Program("Пусть говорят");
+        Tv tv = new Tv("Samsung");
 
+        Channel channel1 = new Channel("Первый");
+        Program p1 = new Program("Фильм");
+        Program p2 = new Program("Новости");
 
+        channel1.addProgram(p1);
+        channel1.addProgram(p2);
+
+        Channel channel2 = new Channel("Второй");
+        Program p3 = new Program("Спорт");
+        Program p4 = new Program("Докментальное кино");
+
+        channel2.addProgram(p3);
+        channel2.addProgram(p4);
+
+        ArrayList<Channel> channels = new ArrayList<>();
+        channels.add(channel1);
+        channels.add(channel2);
+
+        tv.addChannel(channels);
+
+        Controller controller = new Controller();
+        controller.turnTvOn(tv);
+        controller.turnRandomProgram(tv);
 
     }
 }
--- src/HomeworkTask1/Program.java
+++ src/HomeworkTask1/Program.java
@@ -3,14 +3,15 @@ package HomeworkTask1;
 public class Program {
     private String name;
 
-    public Program(String name){
+    public Program(String name) {
         this.name = name;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public String getName(){
-        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
